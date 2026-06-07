b3cec3969a6cff4d9010f29621ea2a739e0510fd
Sabinpabt23 <sabinpant100@gmail.com>
2026-06-07 11:50:28 +0545

    Register conversation and message routes in aggregator

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,10 +7,14 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
+import conversationRoutes from './conversation.routes';
+import messageRoutes from './message.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/conversations', conversationRoutes);
+router.use('/', messageRoutes);
 
 export default router;
