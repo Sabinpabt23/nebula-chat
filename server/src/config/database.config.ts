8ea78465f96c03fc845e1dce74a7bff5460d7c86
Sabinpabt23 <sabinpant100@gmail.com>
2026-06-11 12:48:57 +0545

    test123

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     type: 'postgres',
     url: env.database.url,
     ssl: env.database.ssl ? { rejectUnauthorized: false } : false,
-    synchronize: false,
+    synchronize: true,
     logging: env.nodeEnv === 'development' ? ['error', 'warn'] : ['error'],
     entities: env.nodeEnv === 'production'
         ? ['dist/entities/**/*.js']
